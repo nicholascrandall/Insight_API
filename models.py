8ce0569fca7c321d996d04eca6b9557b70f02091
Nicholas <nickycrandall@gmail.com>
2021-05-17 18:31:25 -0400

    added user model

--- models.py
+++ models.py
@@ -1,5 +1,19 @@
 from peewee import *
-import datetime
+from flask_login import UserMixin
 
 DATABASE = SqliteDatabase('insight.sqlite')
 
+class User(UserMixin, Model):
+    username=CharField(unique=True)
+    email=CharField(unique=True)
+    password=CharField()
+    
+    class Meta:
+        database = DATABASE
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User], safe=True)
+    print("Connected to the DB and created unique tables")
+    
+    DATABASE.close()
