7238bda80a0fb9de4e6e2ce911023f54821cd332
Nicholas <nickycrandall@gmail.com>
2021-05-20 22:25:24 -0400

    added follow list to user

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class User(UserMixin, Model):
     username=CharField(unique=True)
     email=CharField(unique=True)
     password=CharField()
+    follow_list=CharField()
     
     class Meta:
         database = DATABASE
